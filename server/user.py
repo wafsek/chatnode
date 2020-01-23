358e9a40bf460fc1b67db8b5aa7e9d937e21b5d1
wafsek <saraibaljit@gmail.com>
2020-01-23 21:48:16 +0100

    removed user

--- server/user.py
+++
@@ -1,8 +0,0 @@
-class User:
-    """A User class"""
-
-    def __init__(self, email, password, username):
-        self.email = email
-        self.password = password
-        self.username = username
-
